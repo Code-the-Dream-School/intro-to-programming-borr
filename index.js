2f17541e5ea81171962a9fa75dded9091bc939ea
tgeeta <104473710+tgeeta@users.noreply.github.com>
2022-06-06 15:13:41 -0400

    Create index.js
    addEventListener function for message form!

---
+++ index.js
@@ -0,0 +1,19 @@
+const links = document.querySelectorAll('.link');
+const sections = document.querySelectorAll('section');
+
+var activeLink = 0;
+
+links.forEach((link, i) => {
+    link.addEventListener('click', () => {
+        if(activeLink != i){
+            links[activeLink].classList.remove('active');
+            links[activeLink].classList.add('active');
+            sections[activeLink].classList.remove('active');
+
+            setTimeout(() => {
+                activeLink = i;
+                sections[i].classList.add('active');
+            }, 1000);
+        }
+    })
+})   
