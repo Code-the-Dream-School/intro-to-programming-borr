e1a060e991186ac5c2cb80e2ca1384aa2dfe09ac
Pramote S <pramote.suewiroj@gmail.com>
2022-07-24 15:44:58 -0700

    Made additional revision on Jul 24

--- js/index.js
+++ js/index.js
@@ -3,11 +3,13 @@ let thisYear = today.getFullYear();
 const container = document.querySelector('#container');
 const footer = document.querySelector('footer');
 const copyright = document.createElement('copyright');
-copyright.innerHTML = 'Pramote'+thisYear;
-document.footer.appendChild(copyright);
+copyright.innerHTML = 'Pramote '+thisYear;
+footer.appendChild(copyright);
 const skills = ['financial analyst', 'risk management'];
-const skillsSection = getElementById('skills');
+const skillsSection = document.getElementById('skills');
 const skillsList = document.querySelector("h2");
 for (let num in skills) {
-  document.createElement(skills[num]);
+  const newListItem = document.createElement("li");
+  newListItem.innerText = skills[num];
+  skillsSection.appendChild(newListItem);
 }
