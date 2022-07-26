9c71316968b6c047ac039b3cfbe34242c351eeb7
James H Nguyen <108825689+curiousBellyButton@users.noreply.github.com>
2022-07-26 21:36:28 +0700

    lesson-4-2 completed v2

--- js/index.js
+++ js/index.js
@@ -8,9 +8,19 @@ const copyright = document.createElement('p');
 copyright.textContent = `James H. Nguyen ${thisYear}`;
 footer.appendChild(copyright);
 
+const skills = ["HTML", "CSS", "JS", "REACT"];
+const skillsSection = document.getElementById('skills');
 
-console.log(copyright);
+const skillsList = skillsSection.querySelector('ul');
 
+for(i = 0; i < skills.length; i++) {
+  const skill = document.createElement('li');
+  skill.textContent=skills[i];
+  skillsList.appendChild(skill); 
 
-console.log(thisYear);
+};
+
+console.log(skillsList);
+
+// console.log(thisYear);
 
