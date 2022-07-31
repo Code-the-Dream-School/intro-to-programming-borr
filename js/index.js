ba744172e3067570e19397102e901b4ff45f546c
Pramote S <pramote.suewiroj@gmail.com>
2022-07-31 14:01:58 -0700

    Exercise 4.3 v2

--- js/index.js
+++ js/index.js
@@ -13,8 +13,19 @@ for (let num in skills) {
   newListItem.innerText = skills[num];
   skillsSection.appendChild(newListItem);
 }
-const messageForm = document.querySelector('leave_message');
-const btn = document.querySelector('#btn');
-btn.addEventListener('click', function (name) {
-  console.log(name.target);
+const messageForm = document.queryElementByName('leave_message');
+messageForm.addEventListener('submit', function (e) {
+  e.preventDefault() 
+  const formFields = e.target
+  console.log(formFields.name.value);
+  console.log(formFields.email.value);
+  console.log(formFields.message.value);
+  const messageSection = document.queryElementById('messages');
+  const messageList = messageSection.queryElementByTagName('ul');
+  const newMessage = messageList.createElement("li");
+  newMessage.innerText = <a href="mailto:email ">name</a><span>message</span>;
+  const removeButton = document.createElement('button');
+  removeButton.innerText('remove');
+
+  document.getElementById('leave_message').reset();
 };
