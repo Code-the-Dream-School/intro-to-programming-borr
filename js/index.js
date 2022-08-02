a6d2d95d9e2f233ca265eef9cd624bff4b4f02bd
James H Nguyen <108825689+curiousBellyButton@users.noreply.github.com>
2022-08-02 11:14:21 +0700

    L4-3 completed w form validation, hide messages, edit button

--- js/index.js
+++ js/index.js
@@ -22,5 +22,91 @@ for(i = 0; i < skills.length; i++) {
 
 console.log(skillsList);
 
-// console.log(thisYear);
 
+// v3 works
+const messageForm = document.querySelector('[name="leave_message"]');
+
+// add event listner on submit and capture 3 values in the form
+messageForm.addEventListener("submit", function(e) {
+  // console.log(e);
+  
+  e.preventDefault();
+  
+  // log the value of the form fields
+  nameValue = e.target.name.value;
+  console.log(nameValue);
+  emailValue = e.target.email.value;
+  console.log(emailValue);
+  const messageValue = e.target.message.value;
+  console.log(messageValue);
+
+  // validate forms
+  if(nameValue === ''  || emailValue === '' || messageValue === '') {
+    alert("Please fill out all fields");
+    return false      
+  };
+
+  const messageSection = document.querySelector('#messages');
+  const messageList = messageSection.querySelector('ul');
+
+  // hide messages by default
+  messageSection.style.display = 'none';
+
+ 
+  // show emailto link using name, and show the message
+  const newMessage = document.createElement('li');
+  newMessage.innerHTML=`<a style="color:orange;" href="mailto:${emailValue}">${nameValue}</a> <span> wrote: ${messageValue}</span>`; 
+  // newMessage.innerText=`<span>${messageValue}</span>`; 
+
+  // crate removeButton
+  // <button type="button">Click Me!</button>
+  const removeButton = document.createElement('button');
+  removeButton.innerText = "remove";
+  removeButton.type = "button";
+
+  // add the remove button to newMessage
+  newMessage.appendChild(removeButton);
+
+  // add newMessage to messageList
+  messageList.appendChild(newMessage);
+
+    // click remove Button to delete each newMessage
+    removeButton.addEventListener('click', function (){
+      // button's parent element
+      const entry = removeButton.parentNode;
+      // console.log(`removeButton previousSibling:` + removeButton.previousSibling);
+      // console.log(`removeButton getparentNode:` + removeButton.parentNode);
+      entry.remove();
+    })  
+  
+  // add edit button
+  const editButton = document.createElement('button');
+  editButton.innerText = "edit";
+  editButton.type = "button";
+  
+  // add edit button to newMessage
+  newMessage.appendChild(editButton);
+  
+  // click editButton to repopulate the 3 message fields with the same values and possibly remove the existing submitted message
+
+    editButton.addEventListener('click', function() {
+      messageForm.name.value = nameValue;
+      messageForm.email.value = emailValue;
+      messageForm.message.value = messageValue;      
+    });
+
+
+
+  console.log(messageSection);  
+  console.log(newMessage);
+  console.log(removeButton);
+
+  // check if messageSection is empty, hidden by default in index.html
+    messageSection.style.display =  messageList.hasChildNodes() ? "block": "none";
+  
+  messageForm.reset();
+
+});
+
+
+// <a href="mailto:johndoe@fakeemail.com">Example mailto link</a>
