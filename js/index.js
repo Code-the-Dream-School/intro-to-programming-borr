18bdbcf77b63313a94e6ed86af2d47c3c8268e11
Pramote S <pramote.suewiroj@gmail.com>
2022-08-01 21:09:00 -0700

    Exercise 4.3 v4

--- js/index.js
+++ js/index.js
@@ -13,25 +13,28 @@ for (let num in skills) {
   newListItem.innerText = skills[num];
   skillsSection.appendChild(newListItem);
 }
-const messageForm = document.queryElementByName('leave_message');
+const messageForm = document.getElementById('leave_message');
 messageForm.addEventListener('submit', function(e) {
   e.preventDefault(); 
   const formFields = e.target
+  const name = formFields.name.value;
+  const email = formFields.email.value;
+  const message = formFields.message.value;
   console.log(formFields.name.value);
   console.log(formFields.email.value);
   console.log(formFields.message.value);
-  document.getElementById('leave_message').reset();
-})
-const messageSection = document.queryElementById('messages');
-const messageList = messageSection.queryElementByTagName('ul');
-const newMessage = messageList.createElement('li');
-newMessage.innerHTML = "<a href=mailto:"+email+"></a><span>"+message+"</span>";
-const removeButton = document.createElement('button');
-removeButton.innerText = 'remove';
-removeButton.type = 'button';
-removeButton.addEventListener('click', function(e) {
-  const entry = messagesSection.parentNode;
-  entry.remove();
+  const messageSection=document.getElementById('messages');
+  const messageList = messageSection.getElementsByTagName('ul')[0];
+  const newMessage = document.createElement('li');
+  newMessage.innerHTML = "<a href=mailto:"+email+"></a><span>"+message+"</span>";
+  const removeButton = document.createElement('button');
+  removeButton.innerText = 'remove';
+  removeButton.type = 'button';
   newMessage.appendChild(removeButton);
   messageList.append(newMessage);
-});
+  removeButton.addEventListener('click', function(e) {
+   const entry = e.target.parentNode;
+   entry.remove();
+  });
+  document.getElementById('leave_message').reset();
+})
