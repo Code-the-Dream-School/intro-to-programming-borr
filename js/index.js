2403b42bcc37f3711f760cbdb46a63fdff380333
Alejandro.pc <alejandropatinoc@gmail.com>
2022-08-05 09:57:17 -0700

    included a message section in the index.html file. Using DOM, index.js we included text and button handeling.

--- js/index.js
+++ js/index.js
@@ -2,14 +2,14 @@
 let today = new Date();
 let thisYear = today.getFullYear();
 
-/* DOM selection: appending to the footer element */
+/* appending to the footer element */
 const footer = document.querySelector("#footer");
 const content = document.createElement("p");
 
 content.textContent = "Alejandro " + thisYear;
 footer.appendChild(content);
 
-/* DOM selection: appending array of "skills" to unordered list */
+/* appending array of "skills" to unordered list */
 let skills = [
   "Bilingual",
   "Ambitious",
@@ -29,3 +29,40 @@ for (let i = 0; i < skills.length; i++) {
   skill.textContent = skills[i];
   skillsList.appendChild(skill);
 }
+
+/* message form handeling */
+const messageForm = document.getElementsByName("leave_message");
+
+messageForm[0].addEventListener("submit", (doc) => {
+  doc.preventDefault();
+
+  const name = doc.target.name.value;
+  const email = doc.target.email.value;
+  const message = doc.target.message.value;
+
+  console.log(name, email, message);
+
+  /* Displaying message list */
+  const messageSection = document.getElementById("#messeges");
+  const messageList = document.querySelector("#messageList");
+
+  const newMessage = document.createElement("li");
+
+  newMessage.innerHTML = `<a href="mailto: ${email}">${name}</a>
+   <span>wrote: ${message} </span>`;
+
+  /* button handeling */
+  const removeButton = document.createElement("button");
+  removeButton.innerText = "remove";
+  removeButton.setAttribute("type", "button");
+
+  removeButton.addEventListener("click", () => {
+    const entry = removeButton.parentNode;
+    entry.remove();
+  });
+
+  newMessage.appendChild(removeButton);
+  messageList.appendChild(newMessage);
+
+  messageForm[0].reset();
+});
