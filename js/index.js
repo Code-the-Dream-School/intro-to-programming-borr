f0b5afb75937d942a4a6b3bf2129de2916099b87
Geeta <gtturumella@gmail.com>
2022-08-11 01:40:24 -0400

    created CSS folder and file

--- js/index.js
+++ js/index.js
@@ -28,3 +28,34 @@ copyrightFooter.innerHTML = `&copy; Geeta Turumella ${thisYear}`
         listItem.innerHTML = skillsArray[i];
 	orderedList.appendChild(listItem);
      }
+// FORM SECTION
+     const messageForm = document.querySelector('form[name=leave_message]');
+   messageForm.addEventListener('submit', (event) => {
+   event.preventDefault();
+   const name = event.target.name;
+   const email = event.target.email;
+   const message = event.target.message;
+   console.log(name);
+   console.log(email);
+   console.log(message);
+
+   const messageSection = document.querySelector('#messages');
+     const messageList = messageSection.querySelector('ul');
+     const newMessage = document.createElement('li');
+     newMessage.classList.add('list__item');
+     messageList.appendChild(newMessage);
+     newMessage.innerHTML =`<div><a href="mailto:${email.value}">${name.value}</a> &nbsp <span>${message.value}</span></div>`
+     messageForm.reset();
+     
+   // Creation of Remove Button
+     const removeButton = document.createElement('button');
+       removeButton.innerText = 'Remove';
+       removeButton.type = 'button';
+       removeButton.classList.add('button');
+       newMessage.appendChild(removeButton);
+   });
+   //To remove the entry: This not working
+     removeButton.addEventListener('click', (event) => {
+       const entry = event.target.parentNode;
+       entry.remove();
+     });
