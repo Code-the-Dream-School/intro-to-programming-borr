7ef6f4b67bc27e4e5480cfd2cc810238c62f8843
AlekseyK <aleksey.kabishau@icloud.com>
2022-08-15 08:55:28 -0400

    feat: handle message form submit

--- js/index.js
+++ js/index.js
@@ -22,3 +22,16 @@ for (item of skills) {
     skillsList.appendChild(skill);
 
 }
+
+// handle message form submit
+const messageForm = document.getElementsByName('leave_message')[0];
+//console.log(messageForm);
+messageForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    console.log(event.target);
+    const name = event.target.name;
+    const email = event.target.email;
+    const message = event.target.message;
+    console.log(`${name.value} + ${email.value} + ${message.value}`);
+    event.reset();
+})
